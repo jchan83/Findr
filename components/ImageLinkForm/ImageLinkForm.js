d0df3ba5e720ef52fb31d3f05b06d348b11da894
jchan83 <78185367+jchan83@users.noreply.github.com>
2021-06-15 22:32:31 +0800

    Add files via upload

--- components/ImageLinkForm/ImageLinkForm.js
+++ components/ImageLinkForm/ImageLinkForm.js
@@ -2,16 +2,18 @@ import React from 'react';
 import './ImageLinkForm.css';
 
 
-const ImageLinkForm =() => {
+const ImageLinkForm =({ onInputChange, onSubmit }) => {
     return(
     <div>
-        <p className='f3'>
-            {'This oracle will fulfill your craving'}
+        <p className='f3 font'>
+            {'This crystal ball will fulfill your craving'}
         </p>
         <div className='center'>
             <div className='form center pa4 br3 shadow-5'>
-                <input className='f4 pa2 w-70 center' type='tex'/>
-                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'> Detect </button>
+                <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
+                <button className='font w-30 grow f4 link ph3 pv2 dib black bg-light-purple'
+                onClick ={onSubmit}>
+                 Ask!</button>
             </div>
         </div>
     </div>
--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -6,8 +6,8 @@ import ball from './crystalball.png'
 const Logo = () => {
     return (
         <div>
-            <Tilt className="Tilt br2 shadow-3" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
-                <div className="Tilt-inner pa3"> <img style={{paddingTop: '5px'}}alt='crystalball' src={ball}/> </div>
+            <Tilt className="Tilt br2 center" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner pa3"> <img style={{paddingTop: '5px', height: 100}}alt='crystalball' src={ball}/> </div>
             </Tilt>
         </div>
     )
